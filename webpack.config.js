998371a000480791592c1f096dded3ef39ea857b
alrudolph <alex3r@me.com>
2022-02-25 15:13:15 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/site/index.js',
+  output: {
+    filename: 'pol-contr-bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      name: 'pol-contr',
+      filename: '../index.html',
+      template: 'src/site/index.ejs',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = () => config;
